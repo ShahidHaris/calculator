a1ecfc7b5bbcb63dce8fd5b61e6cc37eeda3f543
Shahidharith <alharithshaheed@yahoo.com>
2022-08-26 07:55:50 +0100

    display the number on the console

--- script.js
+++ script.js
@@ -1,4 +1,56 @@
-const numberKeys = document.quer
+const numberKeys = document.querySelectorAll('[data-number]');
+const functionKeys = document.querySelectorAll('[data-function]');
+const deleteKey = document.querySelector('[data-delete]');
+const clearKey = document.querySelector('[data-clear]');
+const equalsKey = document.querySelector('[data-equals]');
+const mainConsole = document.querySelector('[data-main-console]');
+const resultConsole = document.querySelector('[data-result-console]');
+
+
+
+let func;
+let num;
+
+numberKeys.forEach(key => {
+    key.addEventListener('click', () => {
+        mainConsole.textContent = key.textContent;
+        //mainConsole.append(num);
+    })
+})
+
+functionKeys.forEach(key => {
+    key.addEventListener('click', () => {
+        let console = mainConsole.textContent;
+        var lastChar = console[console.length - 1];
+        if (console == '') {
+            return;
+        } else if (lastChar == '+' || lastChar == '-' || lastChar == 'x' || lastChar == '÷') {
+            return;
+        } else {
+            func = num + key.textContent;
+            mainConsole.append(func);
+        }
+        resultConsole.textContent = func;
+    })
+})
+
+clearKey.addEventListener('click', clear)
+deleteKey.addEventListener('click', deleteEntry)
+
+function clear() {
+    mainConsole.textContent = '';
+    resultConsole.textContent = '';
+}
+
+function deleteEntry() {
+    let deleted = mainConsole.textContent;
+    deleted.substring(0, deleted.length - 1);
+}
+
+
+
+
+
 
 
 
@@ -18,6 +70,16 @@ function divide(a, b) {
     return a / b;
 };
 
-function operate(operator, a , b) {
-    add(a, b)
+function operate(operator, a, b) {
+    if (operator == '+') {
+        return add(a, b)
+    } else if (operator == '-') {
+        return subtract(a, b)
+    } else if (operator == '*') {
+        return multiply(a, b)
+    } else if (operator == '÷') {
+        return divide(a, b)
+    } else {
+        return;
+    }
 }
